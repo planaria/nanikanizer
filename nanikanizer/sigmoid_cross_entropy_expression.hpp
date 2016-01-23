16eaed2d6c18e620e590d7b005cc04cbf2d2b73f
Suzuki Kojiro <pn.suzuki.kojiro@gmail.com>
2016-01-23 13:14:06 +0900

    Fix sigmoid_cross_entropy

--- nanikanizer/sigmoid_cross_entropy_expression.hpp
+++ nanikanizer/sigmoid_cross_entropy_expression.hpp
@@ -12,18 +12,18 @@ namespace nnk
 		static T forward(T x)
 		{
 			if (x < static_cast<T>(0.0))
-				return -std::log1p(std::exp(x));
+				return std::log1p(std::exp(-x));
 			else
-				return -std::log1p(std::exp(-x));
+				return std::log1p(std::exp(x));
 		}
 
 		template <class T>
 		static void backward(T& x_grad, T x, T /*y*/, T y_grad)
 		{
 			if (x < static_cast<T>(0.0))
-				x_grad += y_grad * (sigmoid(-x) - static_cast<T>(1.0));
+				x_grad -= y_grad * sigmoid(-x);
 			else
-				x_grad += y_grad * sigmoid(-x);
+				x_grad += y_grad * sigmoid(x);
 		}
 
 	};
