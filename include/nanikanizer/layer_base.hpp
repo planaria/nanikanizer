facc6fdf434416dbaeaf714589a64db1ad3be005
Suzuki Kojiro <pn.suzuki.kojiro@gmail.com>
2016-02-03 21:45:58 +0900

    Make layer_base non-template

--- include/nanikanizer/layer_base.hpp
+++ include/nanikanizer/layer_base.hpp
@@ -4,21 +4,15 @@
 namespace nnk
 {
 
-	template <class T>
 	class layer_base : boost::noncopyable
 	{
 	public:
 		
-		typedef T scalar_type;
-		typedef std::valarray<scalar_type> tensor_type;
-
 		virtual ~layer_base()
 		{
 		}
 
 		virtual void enumerate_parameters(optimizer_base& optimizer) = 0;
-
-		virtual expression<scalar_type> forward(const expression<scalar_type>& v) const = 0;
 
 	};
 
